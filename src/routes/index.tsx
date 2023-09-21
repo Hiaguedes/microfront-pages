b3d21a4a2e99dd182e5fa26a0c1890af142a5550
Hiaguedes <hiago@contasimples.com>
2023-09-21 06:31:34 -0300

    refactor: retorna construtor da pagina

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Page1 from "@src/pages/Page1";
 import Page2 from "@src/pages/Page2";
 
 const routes = [
-    { path: '/teste-1', name: 'Teste 1', element: Page1 },
-    { path: '/teste-2', name: 'Teste 2', element: Page2 }
+    { path: 'teste-1', name: 'Teste 1', element: <Page1 /> },
+    { path: 'teste-2', name: 'Teste 2', element: <Page2 /> }
 ];
 
 export default routes;
